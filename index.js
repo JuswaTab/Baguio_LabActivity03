38134a5d75c7f3f9f0ead10fa226aab7868890e1
JuswaTab <baguio.joshua2004@gmail.com>
2024-09-28 13:42:31 +0800

    Baguio, Joshua LabActivity03

--- index.js
+++ index.js
@@ -9,16 +9,19 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 app.use((err, req, res, next) => {
-    console.error(err.stack); 
-    res.status(500).json({ error: 'Something went wrong!' }); 
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
 });
 
-//array to store user data
+
 const users = [];
 
-// Route to handle GET requests
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
 app.get('/users', (req, res) => {
-    console.log('GET /users endpoint was accessed'); 
+    console.log('GET /users endpoint was accessed');
     res.status(200).json(users);
 });
 
@@ -26,13 +29,36 @@ app.get('/users', (req, res) => {
 // Route to handle POST requests
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
+
+
+    if (!name || name.length < 3) {
+        return res.status(400).json({ error: 'Name is required and must be at least 3 characters long.' });
+    }
+
+
+    if (!email || !emailRegex.test(email)) {
+        return res.status(400).json({ error: 'A valid email is required.' });
+    }
+
+
+    if (!password || password.length < 6) {
+        return res.status(400).json({ error: 'Password is required and must be at least 10 characters long.' });
+    }
+
+
+    const existingUser = users.find(user => user.email === email);
+    if (existingUser) {
+        return res.status(409).json({ error: 'Email already exists.' });
+    }
+
+    
+    
     users.push({ name, email, password });
-    console.log(`POST /users endpoint was accessed ${JSON.stringify(users)}`);
+    console.log(`POST /register endpoint was accessed ${JSON.stringify(users)}`);
     res.status(201).json({ message: 'User registered successfully' });
 });
 
-// Start the server and listen on the specified port
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-    
